d4f0fc687772311698505455755988755166a648
윤진환 <127274711+yunjinhwa@users.noreply.github.com>
2025-12-11 09:36:32 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 푸터의 연도 자동 업데이트
+document.addEventListener("DOMContentLoaded", function () {
+  var yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
